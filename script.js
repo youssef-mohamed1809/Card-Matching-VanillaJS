a5f138595715325d32a98c59384d939d5ed9be66
Youssef Mohamed <youssef.mohamed1809@gmail.com>
2024-05-24 22:43:48 +0300

    Basic card matching functionality(might change later)

--- script.js
+++ script.js
@@ -1,3 +1,59 @@
-function printHi(){
-    console.log("Hi");
+init();
+
+
+
+function init(){
+
+    first_selected = null;
+    cards = document.querySelectorAll("div.card");
+    addNums(cards);
+
+    for(let i = 0; i < cards.length; i++){
+        cards[i].addEventListener("click", () => {
+            card_selected(cards[i].innerHTML);
+        })
+    }
+}
+
+
+function addNums(cards){
+
+    var usedCount = [];
+
+    for(var i = 0; i < cards.length; i++){
+        usedCount[i] = 0;
+    }
+
+    for(var i = 0; i < cards.length; i++){
+        var num = Math.floor(Math.random() * 8) + 1;
+
+        while(usedCount[num - 1] == 2){
+            num = Math.floor(Math.random() * 8) + 1;
+        }
+        usedCount[num - 1]++;
+        cards[i].innerHTML = num;
+    }
+
+}
+
+
+function card_selected(num){
+    if(first_selected == null){
+        //This is the first one selected
+        first_selected = num;
+    }else{
+        //There is one already selected
+        if(first_selected == num){
+            console.log("Suii");
+            for(var i = 0; i < cards.length; i++){
+                if(cards[i].innerHTML == num){
+                    cards[i].remove();
+                }
+            }
+
+        }else{
+            console.log("Wrong");
+        }
+        first_selected = null;
+    }
 }
