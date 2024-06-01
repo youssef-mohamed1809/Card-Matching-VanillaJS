68229509dc810661fd19e089d8676866675f0d51
Youssef Mohamed <youssef.mohamed1809@gmail.com>
2024-06-02 00:26:15 +0300

    Card front and back

--- script.js
+++ script.js
@@ -1,60 +1,69 @@
 init();
 
 function init(){
-    first_selected = null;
     cards = document.querySelectorAll("div.card");
-    addNums(cards);
+    front_cards = document.querySelectorAll("div.front");
+    back_cards = document.querySelectorAll("div.back");
+    first_selected = null;
+    
+    addNums(front_cards);
 
     for(let i = 0; i < cards.length; i++){
+
         cards[i].addEventListener("click", () => {
-            card_selected(cards[i].innerHTML);
+            card_selected(cards[i]);
         })
     }
 }
 
 
-function addNums(cards){
+function addNums(front_cards){
 
     var usedCount = [];
 
-    for(var i = 0; i < cards.length; i++){
+    for(var i = 0; i < front_cards.length; i++){
         usedCount[i] = 0;
     }
 
-    for(var i = 0; i < cards.length; i++){
+    for(var i = 0; i < front_cards.length; i++){
         var num = Math.floor(Math.random() * 8) + 1;
-
         while(usedCount[num - 1] == 2){
             num = Math.floor(Math.random() * 8) + 1;
         }
         usedCount[num - 1]++;
-        // cards[i].innerHTML = num;
         myimg = document.createElement("img");
         myimg.setAttribute("src", `cards/${num}.png`);
-        myimg.setAttribute("style", "height:140px;width:100px;")
-        cards[i].appendChild(myimg);
+        myimg.setAttribute("alt", `${num}`);
+        front_cards[i].setAttribute("style", "display: none;")
+        front_cards[i].appendChild(myimg);
     }
 
 }
 
 
-function card_selected(num){
-    if(first_selected == null){
-        //This is the first one selected
-        first_selected = num;
-    }else{
-        //There is one already selected
-        if(first_selected == num){
-            console.log("Suii");
-            for(var i = 0; i < cards.length; i++){
-                if(cards[i].innerHTML == num){
-                    cards[i].remove();
-                }
-            }
-
-        }else{
-            console.log("Wrong");
+function card_selected(card){
+    var my_num = card.children;
+    for(var i = 0; i < my_num.length; i++){
+        if(my_num.className == "back"){
+           card.classList.toggle('flip');
         }
-        first_selected = null;
     }
+    // if(first_selected == null){
+    //     //This is the first one selected
+    //     first_selected = num;
+    // }else{
+    //     //There is one already selected
+    //     if(first_selected == num){
+    //         console.log("Suii");
+    //         for(var i = 0; i < cards.length; i++){
+    //             if(cards[i].innerHTML == num){
+    //                 cards[i].remove();
+    //             }
+    //         }
+
+    //     }else{
+    //         console.log("Wrong");
+    //     }
+    //     first_selected = null;
+    // }
 }
