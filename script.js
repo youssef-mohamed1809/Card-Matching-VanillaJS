a1959e3ad93029edb631447bffffa17beb21542d
Youssef Mohamed <youssef.mohamed1809@gmail.com>
2024-06-03 23:29:47 +0300

    Basic game functionality done

--- script.js
+++ script.js
@@ -4,66 +4,91 @@ function init(){
     cards = document.querySelectorAll("div.card");
     front_cards = document.querySelectorAll("div.front");
     back_cards = document.querySelectorAll("div.back");
-    first_selected = null;
+    first_card = null;
     
     addNums(front_cards);
 
     for(let i = 0; i < cards.length; i++){
-
         cards[i].addEventListener("click", () => {
             card_selected(cards[i]);
         })
     }
+    game = document.querySelector(".game");
 }
+
+
+// Synchronous Timer Implementation
+// function sleep(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+//   }
+  
+//   async function synchronousTimer(duration, interval) {
+//     const steps = duration / interval;
+//     for (let i = 0; i < steps; i++) {
+//       console.log(`Step ${i + 1}`);
+//       await sleep(interval);
+//     }
+//     console.log('Timer completed');
+//   }
+
 
 
 function addNums(front_cards){
 
-    var usedCount = [];
+    usedCount = [];
 
     for(var i = 0; i < front_cards.length; i++){
         usedCount[i] = 0;
     }
 
-    for(var i = 0; i < front_cards.length; i++){
-        var num = Math.floor(Math.random() * 8) + 1;
+    for(i = 0; i < front_cards.length; i++){
+        num = Math.floor(Math.random() * 8) + 1;
         while(usedCount[num - 1] == 2){
             num = Math.floor(Math.random() * 8) + 1;
         }
         usedCount[num - 1]++;
         myimg = document.createElement("img");
-        myimg.setAttribute("src", `cards/${num}.png`);
+        myimg.setAttribute("src", `assets/cards/${num}.png`);
         myimg.setAttribute("alt", `${num}`);
-        front_cards[i].setAttribute("style", "display: none;")
         front_cards[i].appendChild(myimg);
+
+        
     }
 
 }
 
 
 function card_selected(card){
-    var my_num = card.children;
-    for(var i = 0; i < my_num.length; i++){
-        if(my_num.className == "back"){
-           card.classList.toggle('flip');
-        }
-    }
-    // if(first_selected == null){
-    //     //This is the first one selected
-    //     first_selected = num;
-    // }else{
-    //     //There is one already selected
-    //     if(first_selected == num){
-    //         console.log("Suii");
-    //         for(var i = 0; i < cards.length; i++){
-    //             if(cards[i].innerHTML == num){
-    //                 cards[i].remove();
-    //             }
-    //         }
+    card.classList.toggle("is-flipped");
+    if(first_card == null){
+        first_card = card;
+    }else if(first_card === card){
+        first_card = null;
+    }else{
+        game.classList.toggle("toggle-click");
+        front = first_card.querySelector(".front");
+        front_img = front.querySelector("img");
+        first_num = front_img.getAttribute("alt");
+        
+        front = card.querySelector(".front");
+        front_img = front.querySelector("img");
+        second_num = front_img.getAttribute("alt");
 
-    //     }else{
-    //         console.log("Wrong");
-    //     }
-    //     first_selected = null;
-    // }
+        console.log(first_num);
+        console.log(second_num);
+        setTimeout(()=>{
+            if(first_num == second_num){
+                first_card.remove();
+                card.remove();
+            }else{
+                first_card.classList.toggle("is-flipped");
+                card.classList.toggle("is-flipped");
+            }
+            first_card = null;
+            game.classList.toggle("toggle-click");
+        }, 1000);
+
+        
+    }
+    
 }
